3b80b374c6ce7839f34754211b52c5247c7969d2
Raphael Reis <rapphha@gmail.com>
2021-01-01 15:04:14 -0300

    Teste Domain

---
+++ src/test/java/domain/AnimeTest.java
@@ -0,0 +1,14 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimeTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
